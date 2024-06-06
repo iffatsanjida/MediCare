eca55bc09cc129b297d8aa7e644ce0b1465d61eb
gumpy <asifbhshawon71@gmail.com>
2024-06-06 14:53:26 +0600

    a

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -1,20 +1,57 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 
-public class WelcomePage {
-	JFrame frame = new JFrame();
-	JLabel welcomeLabel = new JLabel("Welcome!");
-
-	WelcomePage(String userID) {
-		welcomeLabel.setBounds(112, 6, 200, 35);
-		welcomeLabel.setText("Welcome, " + userID + "!");
-		welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
-		welcomeLabel.setVerticalAlignment(JLabel.CENTER);
-
-		frame.getContentPane().add(welcomeLabel);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(420, 420);
-		frame.getContentPane().setLayout(null);
-		frame.setVisible(true);
+public class welcomePage extends JFrame {
+private JFrame frame = new JFrame("Menu");
+	
+	public welcomePage() {
+		getContentPane().setBackground(new Color(125, 173, 196));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 967, 585);
+		getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("About");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton.setBackground(new Color(240, 240, 240));
+		btnNewButton.setBounds(36, 31, 85, 21);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Contact");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton_1.setBounds(152, 31, 85, 21);
+		getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Help");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton_2.setBounds(266, 31, 85, 21);
+		getContentPane().add(btnNewButton_2);
+		
+		JLabel lblNewLabel = new JLabel("Welcome");
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 60));
+		lblNewLabel.setBounds(506, 190, 437, 61);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Your problem,we solve");
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1.setBounds(543, 250, 427, 26);
+		getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton_3 = new JButton("Start your journey with us >");
+		btnNewButton_3.setBackground(new Color(192, 192, 192));
+		btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnNewButton_3.setBounds(558, 330, 263, 40);
+		getContentPane().add(btnNewButton_3);
+		
+		
+		
+		
+		setVisible(true);
 	}
 }
